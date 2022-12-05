608882d689ed7f8a54982ba70c5b7df7a08e6d77
Brian McMaster <brian@mcmpest.com>
2022-12-05 12:45:07 -0500

    [IMP] shopfloor_base_default_profile
    Add frontend hook to apply default profile

--- shopfloor_base_default_profile/__manifest__.py
+++ shopfloor_base_default_profile/__manifest__.py
@@ -3,7 +3,7 @@
     "summary": "Adds a default profile mechanism for shopfloor",
     "version": "14.0.1.0.0",
     "development_status": "Alpha",
-    "depends": ["shopfloor_base"],
+    "depends": ["shopfloor_base", "shopfloor_mobile_base"],
     "author": "Brian McMaster, Odoo Community Association (OCA)",
     "maintainers": ["brian10048"],
     "website": "https://github.com/OCA/wms",
@@ -12,7 +12,7 @@
     "installable": True,
     "data": [
         "security/ir.model.access.csv",
-        # "templates/assets.xml",
+        "templates/assets.xml",
         "views/shopfloor_app.xml",
         "views/res_user.xml",
     ],
